1b4e13a8b4cb7a510a5d1c955d4f63205193540a
rickywychoi <cchaff0105@gmail.com>
2022-05-05 12:31:03 -0700

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,147 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { Button } from "react-bootstrap";
+import words from "./words";
+import { useCallback, useMemo, useState } from "react";
+
+const spaceBetween = "3rem";
+
+const horizontalMenu = {
+  display: "flex",
+  justifyContent: "space-around",
+};
+const scoreHeaderStyle = {
+  marginBottom: spaceBetween,
+  ...horizontalMenu,
+};
+
+const scoreTextStyle = {
+  fontSize: "1.5rem",
+};
+
+const wordStyle = {
+  fontSize: "5rem",
+  fontStyle: "bold",
+};
+
+const buttonWrapperStyle = {
+  marginTop: spaceBetween,
+  ...horizontalMenu,
+};
 
 function App() {
+  const [score, setScore] = useState(0);
+  const [pointer, setPointer] = useState(-1);
+  const [team, setTeam] = useState("A");
+
+  const wordsList = useMemo(() => {
+    if (team === "A") return words[0];
+    else if (team === "B") return words[1];
+    else if (team === "C") return words[2];
+    else if (team === "D") return words[3];
+    else if (team === "E") return words[4];
+    else if (team === "F") return words[5];
+    else if (team === "G") return words[6];
+    else if (team === "H") return words[7];
+  }, [team]);
+
+  const isStart = useMemo(() => {
+    return pointer === -1;
+  }, [pointer]);
+
+  const isEnd = useMemo(() => {
+    return pointer === wordsList.length;
+  }, [pointer, wordsList]);
+
+  const proceed = (e) => {
+    if (isEnd) {
+      return;
+    }
+    if (e.target.innerText === "Success") {
+      setScore(score + 1);
+    }
+    setPointer(pointer + 1);
+  };
+
+  const handlePass = (e) => {
+    e.preventDefault();
+    proceed(e);
+  };
+
+  const handleSuccess = (e) => {
+    e.preventDefault();
+    proceed(e);
+  };
+
+  const handleStart = (e) => {
+    e.preventDefault();
+    setPointer(pointer + 1);
+  };
+
+  const handleReset = (e) => {
+    e.preventDefault();
+    setScore(0);
+    setPointer(-1);
+  };
+
+  const handleSwitchTeam = useCallback(
+    (e) => {
+      e.preventDefault();
+      if (team === "A") setTeam("B");
+      else if (team === "B") setTeam("C");
+      else if (team === "C") setTeam("D");
+      else if (team === "D") setTeam("E");
+      else if (team === "E") setTeam("F");
+      else if (team === "F") setTeam("G");
+      else if (team === "G") setTeam("H");
+      else if (team === "H") setTeam("A");
+      handleReset(e);
+    },
+    [team]
+  );
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App" style={{ paddingTop: spaceBetween }}>
+      <span style={scoreHeaderStyle}>
+        <Button variant="outline-primary" onClick={handleSwitchTeam}>
+          Switch Team
+        </Button>
+        <span style={scoreTextStyle}>Team: {team}</span>
+        <span style={scoreTextStyle}>
+          Score: {score}/{wordsList.length}
+        </span>
+      </span>
+      <p style={wordStyle}>
+        {isStart
+          ? "시작?"
+          : isEnd
+          ? "끝"
+          : wordsList.length > 0 && wordsList[pointer]}
+      </p>
+      <>
+        {isStart ? (
+          <span style={buttonWrapperStyle}>
+            <Button variant="success" onClick={handleStart}>
+              Start
+            </Button>
+          </span>
+        ) : (
+          <span style={buttonWrapperStyle}>
+            {!isEnd && (
+              <Button variant="danger" onClick={handlePass}>
+                Pass
+              </Button>
+            )}
+            <Button variant="warning" onClick={handleReset}>
+              Reset
+            </Button>
+            {!isEnd && (
+              <Button variant="primary" onClick={handleSuccess}>
+                Success
+              </Button>
+            )}
+          </span>
+        )}
+      </>
     </div>
   );
 }
